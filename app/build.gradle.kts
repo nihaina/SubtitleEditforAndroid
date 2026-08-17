265c4c409b61e741827ddbd60967ca837c6f75d8
nihaina <z1336579322@gmail.com>
2026-08-17 13:11:37 +0800

    更新1.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,8 +76,8 @@ android {
         applicationId = "com.subtitleedit"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1.4"
+        versionCode = 5
+        versionName = "1.1.5"
 
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
